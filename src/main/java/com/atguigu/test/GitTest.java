3cafd8effc80d96ab4fbaeb0c269675bcaf02713
xsh <xsh@qq.com>
2022-01-15 16:27:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+/**
+ * @ function:
+ * @ author:
+ * @ e-mail:
+ * @ data:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("kuaile");
+    }
+}
